6e5c19f3365e66fe9cdc25b3e93da51b3f0f0a93
dimpurw <terminatorid2@gmail.com>
2021-04-25 21:43:35 +0700

    update controllers auth

--- app/Controllers/Auth/Login.php
+++ app/Controllers/Auth/Login.php
@@ -29,7 +29,7 @@ class Login extends Controller
                     'logged_in'     => TRUE
                 ];
                 $session->set($ses_data);
-                return redirect()->to('/home');
+                return redirect()->to('/users/home');
             }else{
                 $session->setFlashdata('msg', 'Wrong Password');
                 return redirect()->to('/');
