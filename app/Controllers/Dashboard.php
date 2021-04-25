10cee28a910215679426bcc2641ebcc6a3444b4d
dimpurw <terminatorid2@gmail.com>
2021-04-25 21:44:53 +0700

    update controllers

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,5 +1,7 @@
 <?php namespace App\Controllers;
- 
+
+use App\Models\UserModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 use CodeIgniter\Controller;
  
 class Dashboard extends Controller
@@ -7,7 +9,19 @@ class Dashboard extends Controller
     public function home()
     {
         $session = session();
-        $data_nama = $session->get('nama');
-        return view('home', compact('data_nama'));
+        $data['nama'] = $session->get('nama');
+        $data['id'] = $session->get('id');
+        return view('home', $data);
+    }
+
+    public function profile($id)
+	{
+		$users = new UserModel();
+		$data['users'] = $users->where('id', $id)->first();
+		
+		if(!$data['users']){
+			throw PageNotFoundException::forPageNotFound();
+		}
+		echo view('profile', $data);
     }
 }
