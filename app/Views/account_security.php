92165249104f40abf6643304a052722f0c4e27c3
dimpurw <terminatorid2@gmail.com>
2021-05-03 10:57:41 +0700

    views

--- app/Views/account_security.php
+++ app/Views/account_security.php
@@ -30,8 +30,8 @@
 	<div class="row">
 		<div class="col-md-3 ">
 		     <div class="list-group ">
-              <a href="<?= base_url('users/profile/'.$users['id']) ?>" class="list-group-item list-group-item-action active">Profile</a>
-              <a href="<?= base_url('users/account-security/'.$users['id']) ?>" class="list-group-item list-group-item-action">Account security</a>              
+              <a href="<?= base_url('users/profile/'.$users['id']) ?>" class="list-group-item list-group-item-action">Profile</a>
+              <a href="<?= base_url('users/account-security/'.$users['id']) ?>" class="list-group-item list-group-item-action active">Account security</a>              
             </div> 
 		</div>
 		<div class="col-md-9">
@@ -39,40 +39,34 @@
 		        <div class="card-body">
 		            <div class="row">
 		                <div class="col-md-12">
-		                    <h4>Your Profile</h4>
+		                    <h4>Account Security</h4>
 		                    <hr>
 		                </div>
 		            </div>
 		            <div class="row">
 		                <div class="col-md-12">
-		                    <form method="post" action="<?= base_url('users/profile/edit/' . $users['id']) ?>">
+		                    <form method="post" action="">
                               <div class="form-group row">
-                                <label for="nama" class="col-4 col-form-label">Nama</label> 
+                                <label for="password" class="col-4 col-form-label">Password Lama</label> 
                                 <div class="col-8">
-                                  <input id="nama" name="nama" placeholder="nama" class="form-control here" required="required" type="text" value="<?= $users['nama'] ?>">
+                                  <input id="password" name="password" placeholder="Password Lama" class="form-control here" required="required" type="password">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label for="email" class="col-4 col-form-label">Email</label> 
+                                <label for="password" class="col-4 col-form-label">Password Baru</label> 
                                 <div class="col-8">
-                                  <input id="email" name="email" placeholder="email" class="form-control here" type="email" value="<?= $users['email'] ?>">
+                                  <input id="password" name="password" placeholder="Password Baru" class="form-control here" required="required" type="password">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label for="nohp" class="col-4 col-form-label">Nomor Telepon</label> 
+                                <label for="password" class="col-4 col-form-label">Konfirmasi Password Baru</label> 
                                 <div class="col-8">
-                                  <input id="nohp" name="nohp" placeholder="nohp" class="form-control here" type="number" value="<?= $users['nohp'] ?>">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label for="alamat" class="col-4 col-form-label">Alamat</label> 
-                                <div class="col-8">
-                                  <textarea id="alamat" name="alamat" cols="40" rows="4" class="form-control"><?= $users['alamat'] ?></textarea>
+                                  <input id="password" name="password" placeholder="Konfirmasi Password Baru" class="form-control here" required="required" type="password">
                                 </div>
                               </div>
                               <div class="form-group row">
                                 <div class="offset-4 col-8">
-                                  <button name="submit" type="submit" class="btn btn-primary">Update My Profile</button>
+                                  <button name="submit" type="submit" class="btn btn-primary">Update Account Security</button>
                                   <a href="<?= base_url('users/home') ?>"><button name="button" type="button" class="btn btn-warning">Home</button></a>
                                 </div>
                               </div>
