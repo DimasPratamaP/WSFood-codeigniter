df9b7ff30a6da2c1392975633cfc639836a356c8
dimpurw <terminatorid2@gmail.com>
2021-06-27 22:22:22 +0700

    create join table :sparkles:

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -6,4 +6,21 @@ class PostModel extends Model{
     protected $table = 'posts';
     protected $allowedFields = ['users_id','judul','image','deskripsi'];
     
+    function viewPost() 
+    {
+        $db      = \Config\Database::connect();
+        $builder = $db->table('posts');
+        $builder->join('users', 'users.id = posts.users_id');
+        $query = $builder->get()->getResult();
+        return $query;
+    }
+
+    function firstPost()
+    {
+        $db      = \Config\Database::connect();
+        $builder = $db->table('posts');
+        $builder->where('users_id', session('id'));
+        $query = $builder->get()->getResult();
+        return $query;
+    }    
 }
